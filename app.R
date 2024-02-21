c612df0d6a3c51bc01d1181f76a51239ae5245fd
John Coene <jcoenep@gmail.com>
2024-02-21 09:53:44 +0100

    fix: remove cstom ggstatsplot blocks

--- app.R
+++ app.R
@@ -1,10 +1,10 @@
 # Launch the ShinyApp (Do not remove this comment)
-library(ggplot2)
 library(sortable)
 library(blockr.extra)
-library(pharmaversesdtm)
-library(pharmaverseadam)
-library(ggstatsplot)
+library(blockr.ggplot2)
+library(blockr.pharmaverseadam)
+library(blockr.pharmaversesdtm)
+library(blockr.ggstatsplot)
 library(dplyr)
 pkgload::load_all(
   export_all = TRUE,
