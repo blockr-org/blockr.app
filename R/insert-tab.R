fad94b33ab37996391e2d6559d0d4f1e2172b8a9
John Coene <jcoenep@gmail.com>
2024-04-25 12:23:39 +0200

    fix: position of add-stack

--- R/insert-tab.R
+++ R/insert-tab.R
@@ -20,32 +20,38 @@ insert_block_tab <- \(title, input, output, session, locked){
         )
       ),
       div(
-        class = "flex-grow-1",
-        actionButton(
-          add_id,
-          "row",
-          icon = icon("plus"),
-          class = "btn-sm add-row"
-        )
-      ),
-      div(
-        class = "flex-shrink-1 p-1",
-        blockr.ui::addStackUI(
-          add_id, 
-          content = span(
-            class = "rounded border border-secondary px-2 py-1 text-muted",
-            icon("grip"), 
-            "Stack"
-          ),
-          target = ".masonry-row"
-        )
-      ),
-      div(
         class = "flex-shrink-1",
-        tags$a(
-          id = remove_id,
-          class = "remove-tab btn btn-sm locker btn-outline-danger",
-          icon("trash")
+        div(
+          class = "d-flex",
+          div(
+            class = "flex-grow-1",
+            actionButton(
+              add_id,
+              "row",
+              icon = icon("plus"),
+              class = "btn-sm add-row me-2"
+            )
+          ),
+          div(
+            class = "flex-grow-1",
+            blockr.ui::addStackUI(
+              add_id, 
+              content = span(
+                class = "rounded border border-secondary px-2 py-1 text-muted",
+                icon("grip"), 
+                "Stack"
+              ),
+              target = ".masonry-row"
+            )
+          ),
+          div(
+            class = "flex-grow-1",
+            tags$a(
+              id = remove_id,
+              class = "remove-tab btn btn-sm locker btn-outline-danger ms-2",
+              icon("trash")
+            )
+          )
         )
       )
     ),
